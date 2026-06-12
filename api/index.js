9a0913c12c98c12fdec5cc9e22cc53254035401f
Winxx0102 <olowixtovar@gmail.com>
2026-06-11 21:34:16 -0400

    vercel jsonnnnnnn

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+// api/index.js
 const { NestFactory } = require('@nestjs/core');
 const { AppModule } = require('../dist/src/app.module');
 const { ExpressAdapter } = require('@nestjs/platform-express');
@@ -5,13 +6,13 @@ const express = require('express');
 
 const server = express();
 
-// Esta función permite que Vercel ejecute la app
-async function createServer() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  // IMPORTANTE: Esto asegura que todas tus rutas tengan el prefijo /api
+  app.setGlobalPrefix('api'); 
   await app.init();
-  return server;
 }
 
-createServer();
+bootstrap();
 
 module.exports = server;
