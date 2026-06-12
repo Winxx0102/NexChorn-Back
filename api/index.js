81789ba56d0a0913b8689c0d6774af68ff19e82e
Winxx0102 <olowixtovar@gmail.com>
2026-06-11 22:27:35 -0400

    prismaajajasdasd

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
-// api/index.js
 const { NestFactory } = require('@nestjs/core');
 const { AppModule } = require('../dist/src/app.module');
 const { ExpressAdapter } = require('@nestjs/platform-express');
 const express = require('express');
 
-const server = express();
+const expressApp = express();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+const server = async () => {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  app.setGlobalPrefix('api'); // Si tus rutas empiezan con /api, esto es obligatorio
   await app.init();
-}
+  return expressApp;
+};
 
-bootstrap();
-
-module.exports = server;
+module.exports = server();
