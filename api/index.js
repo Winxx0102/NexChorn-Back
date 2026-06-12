cc613a5ab168d15ae6b411a8556bf51b99fbdb39
Winxx0102 <olowixtovar@gmail.com>
2026-06-11 22:38:53 -0400

    prissa

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
-const { createNestServer } = require('../dist/main');
+const { NestFactory } = require('@nestjs/core');
+const { AppModule } = require('../dist/src/app.module');
+const { ExpressAdapter } = require('@nestjs/platform-express');
+const express = require('express');
+
+const expressApp = express();
+let cachedApp;
 
 module.exports = async (req, res) => {
-  const server = await createNestServer();
-  server(req, res);
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    app.setGlobalPrefix('api');
+    await app.init();
+    cachedApp = expressApp;
+  }
+  return cachedApp(req, res);
 };
