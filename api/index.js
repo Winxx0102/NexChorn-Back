8dc7e943d5e5af04813aea08cb847200040af945
Winxx0102 <olowixtovar@gmail.com>
2026-06-11 23:35:32 -0400

    nuevooo

--- api/index.js
+++ api/index.js
@@ -3,29 +3,14 @@ const { AppModule } = require('../dist/src/app.module');
 const { ExpressAdapter } = require('@nestjs/platform-express');
 const express = require('express');
 
-// Creamos la instancia fuera de la exportación para mantenerla en caché (Warm Start)
-let app;
-
-async function bootstrap() {
-  const expressApp = express();
-  
-  // Usamos el adaptador correctamente
-  const nestApp = await NestFactory.create(
-    AppModule, 
-    new ExpressAdapter(expressApp)
-  );
-
-  nestApp.setGlobalPrefix('api');
-  
-  // IMPORTANTE: init() es suficiente, no necesitas escuchar puerto
-  await nestApp.init();
-  
-  return expressApp;
-}
+const expressApp = express();
+let nestApp;
 
 module.exports = async (req, res) => {
-  if (!app) {
-    app = await bootstrap();
+  if (!nestApp) {
+    nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    nestApp.setGlobalPrefix('api');
+    await nestApp.init();
   }
-  return app(req, res);
+  return expressApp(req, res);
 };
