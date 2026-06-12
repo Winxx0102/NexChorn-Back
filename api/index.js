0fba029143cfd195a4f32b150b249081b2177cc5
Winxx0102 <olowixtovar@gmail.com>
2026-06-11 22:37:09 -0400

    pris

--- api/index.js
+++ api/index.js
@@ -1,16 +1,6 @@
-const { NestFactory } = require('@nestjs/core');
-const { AppModule } = require('../dist/src/app.module');
-const { ExpressAdapter } = require('@nestjs/platform-express');
-const express = require('express');
-
-const server = express();
-let app;
+const { createNestServer } = require('../dist/main');
 
 module.exports = async (req, res) => {
-  if (!app) {
-    app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-    app.setGlobalPrefix('api');
-    await app.init();
-  }
-  return server(req, res);
+  const server = await createNestServer();
+  server(req, res);
 };
--- dist/src/chronicles/dto/update-chronicle.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateChronicleDto = void 0;
-const swagger_1 = require("@nestjs/swagger");
-const create_chronicle_dto_1 = require("./create-chronicle.dto");
-class UpdateChronicleDto extends (0, swagger_1.PartialType)(create_chronicle_dto_1.CreateChronicleDto) {
-}
-exports.UpdateChronicleDto = UpdateChronicleDto;
-//# sourceMappingURL=update-chronicle.dto.js.map
