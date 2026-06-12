e8d599e33f1ebdb43509bcb4b051566eca4e8896
Winxx0102 <olowixtovar@gmail.com>
2026-06-12 18:20:17 -0400

    adminpanelfixed

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './user.service';
 import { UsersController } from './user.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { ChroniclesModule } from '@/chronicles/chronicles.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ChroniclesModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService], // Indispensable para que AuthModule lo vea
