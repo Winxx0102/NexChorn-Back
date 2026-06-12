612d26ec7ee07c6bcd5bd0e7abdd27bbc9ac85c7
Winxx0102 <olowixtovar@gmail.com>
2026-06-11 22:46:00 -0400

    prissamsdaa

--- api/index.js
+++ api/index.js
@@ -1,13 +1,23 @@
+
+
 const { NestFactory } = require('@nestjs/core');
 const { AppModule } = require('../dist/src/app.module');
 const { ExpressAdapter } = require('@nestjs/platform-express');
 const express = require('express');
 
 const expressApp = express();
+let app;
 
 module.exports = async (req, res) => {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-  app.setGlobalPrefix('api');
-  await app.init();
-  expressApp(req, res);
+  if (!app) {
+    app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    app.setGlobalPrefix('api');
+    await app.init();
+    // LOGEAR RUTAS PARA DEPURAR
+    const serverRoutes = expressApp._router.stack
+      .filter(r => r.route)
+      .map(r => r.route.path);
+    console.log("Rutas detectadas:", serverRoutes);
+  }
+  return expressApp(req, res);
 };
